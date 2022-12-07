02fee7d873e278a1b4ae9f949b111d5ad9faea6a
Taofiq007 <119982573+Taofiq007@users.noreply.github.com>
2022-12-07 07:02:32 +0600

    Add files via upload

---
+++ class1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+void main()
+
+{
+    int num1,num2;
+    float sum;
+
+    printf("Enter two numbers: " );
+    scanf("%d %d",&num1,&num2);
+
+    sum = num1+num2;
+    printf("%d + %d =%0.1f\n",num1,num2,sum);
+
+    sum = num1 - num2;
+    printf("%d - %d =%0.1f\n",num1,num2,sum);
+
+    sum = num1 * num2;
+    printf("%d * %d =%0.1f\n",num1,num2,sum);
+
+    sum = num1 / num2;
+    printf("%d / %d =%0.1f\n",num1,num2,sum);
+
+
+}
